e355261587880e97366069291782041ceef976ce
nicoleolivera <nicoleolivera08@hotmail.com>
2024-10-18 20:51:59 -0300

    Actualizaciones en html de contenido y nuevo html, en css de bootstrap y venobox minificado, en js de galería (venobox), y archivos de imagen nuevos

---
+++ js/galeria.js
@@ -0,0 +1,9 @@
+new VenoBox({
+    selector: '.my-image-links',
+    numeration: true,
+    infinigall: true,
+    share: true,
+    titleattr: 'data-title',
+    // fitview: true,
+    spinner: 'rotating-plane'
+});
